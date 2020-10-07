6f21645b8f88c0dff164bd802c1b300a2ad2a12c
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-10-07 16:38:45 -0700

    halfway through HW

--- middleware/user.js
+++ middleware/user.js
@@ -1,35 +1,64 @@
-const userDb = require("../users/userDb")
+const userDb = require("../users/userDb");
 
 //custom middleware
 
-function validateUserId() {
+function validateUserId(req, res, next) {
 	return (req, res, next) => {
-userDb.getById(req.params.id)
-.then((user) => {
-    if (user) {
-        next()
-    } else {
-        res.status(404).json({
-            message: "User not found",
-        })
-    }
-})
-.catch((error) => {
-    res.status(500).json({
-        message: "Error retrieving the user"
-    })
-})
-    }
+		userDb
+			.getById(req.params.id)
+			.then((user) => {
+				if (user) {
+					req.user = user;
+					next();
+				} else {
+					res.status(404).json({
+						message: "User not found",
+					});
+				}
+			})
+			.catch((error) => {
+				res.status(500).json({
+					message: "Error retrieving the user",
+				});
+			});
+	};
 }
 
 function validateUser(req, res, next) {
-	// do your magic!
-}
+	return (req, res, next) => {
+		if (!req.body) {
+			return res.status(400).json({
+				message: "missing user data",
+			});
+		} else if (!req.body.name) {
+			return res.status(400).json({
+				message: "missing required name field",
+			});
+		}
+		next();
+	};
 
+	//     validatePost validates the body on a request to create a new post
+	// if the request body is missing, cancel the request and respond with status 400 and { message: "missing post data" }
+	// if the request body is missing the required text field, cancel the request and respond with status 400 and { message: "missing required text field" }
+}
 function validatePost(req, res, next) {
-	// do your magic!
+	return (req, res, next) => {
+		if (!req.body) {
+			return res.status(400).json({
+				message: "missing post data",
+			});
+		} else if (!req.body.text) {
+			return res.status(400).json({
+				message: "missing required text field",
+			});
+		}
+		next();
+	};
 }
 
 module.exports = {
-    validateUserId,
-}
+	validateUserId,
+    validateUser,
+    validatePost
+};
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,36 +1,73 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-
-router.post('/', (req, res) => {
+const userDb = require("./userDb");
+const {
+	validateUserId,
+	validateUser,
+	validatePost,
+} = require("../middleware/user");
+//COMPLETED
+router.post("/", (req, res) => {
+	userDb
+		.insert(req.body)
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.get('/', (req, res) => {
- res.json({
-		message: "Welcome to the API",
- });
-});
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", validateUserId(), validatePost(), (req, res) => {
+  userDb
+		.insert(req.body)
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
 
+router.get("/", (req, res) => {
+	userDb
+		.get()
+		.then(res.status(200).json({ message: "Welcome to the API" }))
+		.catch(res.status(404).json({ message: "does not exist" }));
+});
+
+//COMPLETED
+router.get("/:id", validateUserId(), (req, res) => {
+  userDb
+  .getById(req.user)
+  .then((response) => {res.status(200).json(req.user)})
+.catch((error) => {res.status(500).json({error: "The posts information could not be retrieved."})})  
+});
+
+//COMPLETED
+router.get("/:id/posts", validateUserId(), (req, res) => {
+  userDb
+  .getUserPosts(req.user)
+  .then((response) => {res.status(200).json(response)})
+	.catch((error) => {res.status(500).json({error: "The posts information could not be retrieved."})});
+});
+
+//COMPLETED
+router.delete("/:id", validateUserId(), (req, res) => {
+	userDb
+		.remove(req.user)
+		.then((response) => {
+			res.status(200).json({message: "Deleted"});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+router.put("/:id", validateUserId(), (req, res) => {
+	userDb.update(req.user, req.body).then(res.status(200));
+});
 
 module.exports = router;
