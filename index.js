6f25fd958f39fa9a63c3bc705165489342675e07
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-10-09 14:23:35 -0700

    putting it to bed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express")
 
 const logger = require("./middleware/logger");
 const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
+//const otherServer = require('./server')
 const server = express()
 const port = 4000
 
@@ -12,6 +14,10 @@ server.use(logger("long"))
 server.use(express.json());
 
 server.use(userRouter)
+server.use("/:id/posts", postRouter)
+//server.use(otherServer)
+
+
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,13 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
 const {
 	validateUserId,
 	validateUser,
 	validatePost,
 } = require("../middleware/user");
 //COMPLETED
-router.post("/", (req, res) => {
+router.post("/", validateUser(), (req, res) => {
 	userDb
 		.insert(req.body)
 		.then((response) => {
@@ -18,10 +19,13 @@ router.post("/", (req, res) => {
 			res.status(500).json(error);
 		});
 });
-
+//completed
 router.post("/:id/posts", validateUserId(), validatePost(), (req, res) => {
-  userDb
-		.insert(req.body)
+	postDb
+		.insert({
+			text: req.body.text,
+			user_id: req.body.user_id,
+		})
 		.then((response) => {
 			res.status(200).json(response);
 		})
@@ -30,28 +34,44 @@ router.post("/:id/posts", validateUserId(), validatePost(), (req, res) => {
 		});
 });
 
-
+//COMPLETED
 router.get("/", (req, res) => {
 	userDb
 		.get()
-		.then(res.status(200).json({ message: "Welcome to the API" }))
-		.catch(res.status(404).json({ message: "does not exist" }));
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((error) => {
+			res.status(404).json({ message: "does not exist" });
+		});
 });
 
 //COMPLETED
 router.get("/:id", validateUserId(), (req, res) => {
-  userDb
-  .getById(req.user)
-  .then((response) => {res.status(200).json(req.user)})
-.catch((error) => {res.status(500).json({error: "The posts information could not be retrieved."})})  
+	userDb
+		.getById(req.user)
+		.then((response) => {
+			res.status(200).json(req.user);
+		})
+		.catch((error) => {
+			res
+				.status(500)
+				.json({ error: "The posts information could not be retrieved." });
+		});
 });
 
 //COMPLETED
 router.get("/:id/posts", validateUserId(), (req, res) => {
-  userDb
-  .getUserPosts(req.user)
-  .then((response) => {res.status(200).json(response)})
-	.catch((error) => {res.status(500).json({error: "The posts information could not be retrieved."})});
+	userDb
+		.getUserPosts(req.user)
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res
+				.status(500)
+				.json({ error: "The posts information could not be retrieved." });
+		});
 });
 
 //COMPLETED
@@ -59,15 +79,20 @@ router.delete("/:id", validateUserId(), (req, res) => {
 	userDb
 		.remove(req.user)
 		.then((response) => {
-			res.status(200).json({message: "Deleted"});
+			res.status(200).json({ message: "Deleted" });
 		})
 		.catch((error) => {
 			console.log(error);
 		});
 });
-
+//COMPLETED
 router.put("/:id", validateUserId(), (req, res) => {
-	userDb.update(req.user, req.body).then(res.status(200));
+	userDb
+		.update(req.user.id, { name: req.body.name })
+		.then((response) => res.status(200).json(response))
+		.catch((error) => {
+			res.status(500).json({ error: "Could not update post." });
+		});
 });
 
 module.exports = router;
